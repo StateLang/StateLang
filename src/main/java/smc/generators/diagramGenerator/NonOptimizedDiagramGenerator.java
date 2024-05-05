272a2896a120c9f45a96f56a43205c790a01a1f0
AhmedHamdi0 <ahmed88hamdi99@gmail.com>
2024-05-05 08:56:58 +0300

    feat[generator]: define stateNodes and transitionNodes lists in generate

--- src/main/java/smc/generators/diagramGenerator/NonOptimizedDiagramGenerator.java
+++ src/main/java/smc/generators/diagramGenerator/NonOptimizedDiagramGenerator.java
@@ -6,55 +6,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NonOptimizedDiagramGenerator {
-  private List<DiagramNode.StateNode> stateNodes;
+	private List<DiagramNode.StateNode> stateNodes;
   private List<DiagramNode.TransitionNode> transitionNodes;
 
-	public NonOptimizedDiagramGenerator() {
-		stateNodes = new ArrayList<>();
-		transitionNodes = new ArrayList<>();
-	}
-
   public DiagramNode generate(SemanticStateMachine sm) {
+	  stateNodes = new ArrayList<>();
+	  transitionNodes = new ArrayList<>();
     prepareFsm(sm);
-    return makeFsmNode();
+    return makeFsmNode(sm);
   }
 
   private void prepareFsm(SemanticStateMachine sm) {
-	  addTransition("[*]", "", sm.initialState.name, new ArrayList<>());
 	  for (SemanticStateMachine.SemanticState state : sm.states.values()) {
       addState(state);
-      addTransitions(sm, state);
+      addStateTransitions(state);
     }
   }
 
   private void addState(SemanticStateMachine.SemanticState state) {
     String parentName = state.superStates.isEmpty()
-        ? ""
-        : state.superStates.first().name;
+      ? ""
+      : state.superStates.first().name;
 
     stateNodes.add(new DiagramNode.StateNode(
-        state.name,
-        parentName,
-        state.entryActions,
-        state.exitActions
+      state.name,
+      parentName,
+      state.entryActions,
+      state.exitActions
     ));
   }
 
-  private void addTransitions(SemanticStateMachine sm, SemanticStateMachine.SemanticState state) {
+  private void addStateTransitions(SemanticStateMachine.SemanticState state) {
     for (SemanticStateMachine.SemanticTransition tr : state.transitions)
       addTransition(state.name, tr.event, tr.nextState.name, tr.actions);
   }
 
   private void addTransition(String currentState, String event, String nextState, List<String> actions) {
     transitionNodes.add(new DiagramNode.TransitionNode(
-        currentState,
-        event,
-        nextState,
-        actions
+      currentState,
+      event,
+      nextState,
+      actions
     ));
   }
 
-  private DiagramNode makeFsmNode() {
-    return new DiagramNode.FSMNode(stateNodes, transitionNodes);
+  private DiagramNode makeFsmNode(SemanticStateMachine sm) {
+    return new DiagramNode.FSMNode(sm.initialState.name, stateNodes, transitionNodes);
   }
 }
